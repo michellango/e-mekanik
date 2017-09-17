ab17a5eac6b145c2f45b5c0c0504e57488813e8f
Mohamad Haris Muzadi <harismuzadi@gmail.com>
2017-09-17 21:59:16 +0700

    splash screen berhasil

---
+++ app/src/main/java/com/example/pjbservices/e_mekanik/forminputquestion.java
@@ -0,0 +1,17 @@
+package com.example.pjbservices.e_mekanik;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+/**
+ * Created by PJBServices on 17/09/2017.
+ */
+public class forminputquestion extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forminputquestion);
+
+    }
+}
---
+++ app/src/main/java/com/example/pjbservices/e_mekanik/formview.java
@@ -0,0 +1,7 @@
+package com.example.pjbservices.e_mekanik;
+
+/**
+ * Created by PJBServices on 18/09/2017.
+ */
+public class formview {
+}
